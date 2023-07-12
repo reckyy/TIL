067fc36e72ba6bc382a0a9844615c2ed14603036
reckyy <srreki98@gmail.com>
2023-07-12 22:01:45 +0900

    3-1完了

---
+++ chapter_3.rb
@@ -0,0 +1,19 @@
+#3-1-1
+def less?(number)
+  if number < 365
+    puts "true"
+  else
+    puts "false"
+  end
+end
+less?(2)
+
+#3-1-2
+def equal?(number)
+  if number == (1 + 1)
+    puts "true"
+  else
+    puts "false"
+  end
+end
+equal?(2)
