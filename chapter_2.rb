44f2a2086ec63e7bb5292a02c2df9823174dea8e
reckyy <srreki98@gmail.com>
2023-07-12 21:52:01 +0900

    2-2完了

--- chapter_2.rb
+++ chapter_2.rb
@@ -7,3 +7,13 @@ def find_circle
   puts r * r * 3.14
 end
 find_circle
+
+#2-2-3
+puts "Ruby"
+puts "abc" + "def"
+def change_integer
+  a = "123".to_i
+  b = "456".to_i
+  puts a + b
+end
+change_integer
