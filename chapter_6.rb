880da0f32c0f6ced697411a2466373688e880176
reckyy <srreki98@gmail.com>
2023-07-14 16:14:46 +0900

    6-2完了

--- chapter_6.rb
+++ chapter_6.rb
@@ -1,7 +1,27 @@
 #6-1-1
-menu = {coffee: 300, caffe_latte: 400}
-p menu[:coffee]
+menu1 = {coffee: 300, caffe_latte: 400}
+p menu1[:coffee]
 
 #6-1-2
-menu = {"モカ" => "チョコ", "カフェラテ" => "ミルク入り"}
-p menu["モカ"]
+menu2 = {"モカ" => "チョコ", "カフェラテ" => "ミルク入り"}
+p menu2["モカ"]
+
+#6-2-3
+menu1[:tea] = 300
+p menu1
+
+#6-2-4
+menu1.delete(:coffee)
+p menu1
+
+#6-2-5(unlessメソッドを使えば一行で済んだので、そちらの方がベター)
+if menu1[:coffee]
+  puts "何で？"
+else
+  puts puts "紅茶はありませんか？"
+end
+
+#6-2-6
+p "カフェラテください" unless menu1[:caffe_latte] > 500
+
+#6-2-7
