78e82c873ea06831c3cd9a4945d795e177993a02
reckyy <srreki98@gmail.com>
2023-07-14 16:45:35 +0900

    7-3完了

--- chapter_7.rb
+++ chapter_7.rb
@@ -11,7 +11,26 @@ end
 puts area
 
 #7-2-3
-def dice
-  [1,2,3,4,5,6].sample
+#1以外の場合戻り値がnilになってしまうダメな例
+def bad_dice
+  dice_number = [1,2,3,4,5,6].sample
+  if dice_number == 1
+    puts 'もう一回'
+    [1,2,3,4,5,6].sample
+  end
 end
-puts dice
+def good_dice
+  dice_number = [1,2,3,4,5,6].sample
+  return dice_number unless dice_number == 1
+  puts 'もう一回'
+  dice_number = [1,2,3,4,5,6].sample
+end
+puts good_dice
+
+#7-3-4
+def order2(menu)
+  puts "#{menu}ください"
+end
+order2("モカ")
+
+#7-3-5
