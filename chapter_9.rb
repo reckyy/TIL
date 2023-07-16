56b9455b7a36593d9cc7499474872bf8a1fdc41b
reckyy <srreki98@gmail.com>
2023-07-16 10:20:00 +0900

    9-3完了

--- chapter_9.rb
+++ chapter_9.rb
@@ -25,3 +25,20 @@ module EspressoShot
   Price = 100
 end
 p EspressoShot::Price
+
+#9-3-4
+#ファイル名が123.rbと仮定
+#123.rbの中身----------
+module WhippedCream
+  def self.info
+    "トッピング用ホイップクリーム"
+  end
+end
+#----------------------
+
+#別ファイルの中身
+#-----------
+#require_relative "123"
+#p WhippedCream.info
+
+#-----------
