9c49908acd1187479d4b5dea1ccc1c247c222011
reckyy <srreki98@gmail.com>
2023-08-04 21:40:40 +0900

    to_intsメソッド作成

--- ruby-book/test/rgb_test.rb
+++ ruby-book/test/rgb_test.rb
@@ -6,4 +6,8 @@ class RgbTest < Minitest::Test
     assert_equal '#000000', to_hex(0, 0, 0)
     assert_equal '#043c78', to_hex(4, 60, 120)
   end
+
+  def test_to_ints
+    assert_equal [0, 0, 0], to_ints('#000000')
+  end
 end
