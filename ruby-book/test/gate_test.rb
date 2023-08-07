e758676b33ec818eebc64359a34defcd2ed8d18e
reckyy <srreki98@gmail.com>
2023-08-07 21:59:38 +0900

    case1のテスト作成

--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -3,6 +3,11 @@ require_relative '../lib/gate'
 
 class GateTest < Minitest::Test
   def test_gate
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(160)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
