964a5ce4d5f50bed16ccb259514d90f509959e98
reckyy <srreki98@gmail.com>
2023-08-19 10:03:30 +0900

    ブロックパラメータを変更

--- Ruby/ls.rb
+++ Ruby/ls.rb
@@ -5,9 +5,9 @@ def ls_v1
   total_row = (amari == 0) ? div : (div + 1)
   width = all_file.max { |a, b| a.length <=> b.length }.size + 7
   all_sort_file = all_file.each_slice(3).to_a
-  0.upto(total_row - 1) do |idx|
-    3.times do |time|
-      print all_sort_file[idx][time].ljust(width)
+  total_row.times do |row|
+    3.times do |col|
+      print all_sort_file[row][col].ljust(width)
     end
     puts
   end
