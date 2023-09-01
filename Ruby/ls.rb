1309f48a3c3489995b6b2822236f542139a6df6b
reckyy <srreki98@gmail.com>
2023-09-01 22:38:42 +0900

    メソッドを分岐
    parse_fileメソッドに行数を求めるファイル解析とは関係ない処理が混在していたので、
    それらをcalculateメソッドに分離

--- Ruby/ls.rb
+++ Ruby/ls.rb
@@ -1,38 +1,26 @@
-# frozen_string_literal: true
+require 'debug'
 
 initial_row = 3
 
-def parse_file
+def parse_file(initial_row)
   all_file = Dir.glob('*').sort
+  all_file
+end
+
+def calculate_row_and_space(initial_row, all_file)
   div, mod = all_file.size.divmod(initial_row)
   total_row = mod.zero? ? div : (div + 1)
   width = all_file.max_by(&:length).length + 7
-  [all_file, total_row, width]
+  [total_row, width]
 end
 
-def ls_v1(all_file, total_row, width)
+all_file = parse_file(initial_row)
+total_row, width = calculate_row_and_space(initial_row, all_file)
+
+def ls_v1(initial_row, all_file, total_row, width)
   all_sort_file = all_file.each_slice(total_row).to_a
-  total_row.times do |row|
-    initial_row.times do |col|
-      file_name = all_sort_file[col][row]
-      print file_name.ljust(width) unless file_name.nil?
-    end
-    puts
-  end
-end
-
-all_file, total_row, width = parse_file
-
-ls_v1(all_file, total_row, width)
-
-def ls_v2(all_file, total_row, width)
-  grouped_files = all_file.map.with_index do |file, idx|
-    [idx % total_row, file]
-  end
-  grouped_files = grouped_files.group_by { |div, _| div }
-
-  total_row.times do |row|
-    initial_row.times do |col|
+  total_row.times do |col|
+    initial_row.times do |row|
       file_name = all_sort_file[row][col]
       print file_name.ljust(width) unless file_name.nil?
     end
@@ -40,4 +28,19 @@ def ls_v2(all_file, total_row, width)
   end
 end
 
-ls_v2(all_file, total_row, width)
+def ls_v2(initial_row, all_file, total_row, width)
+  grouped_files = all_file.map.with_index do |file, idx|
+    [idx % total_row, file]
+  end
+  grouped_files = grouped_files.group_by { |div, _| div }
+  total_row.times do |row|
+    initial_row.times do |col|
+      file_name = grouped_files[row][col]
+      print file_name[1].ljust(width) unless file_name.nil?
+    end
+    puts
+  end
+end
+
+ls_v1(initial_row, all_file, total_row, width)
+ls_v2(initial_row, all_file, total_row, width)
