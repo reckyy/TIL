8b431dac8c00101ed6af292b2efb74e5efa16fef
reckyy <srreki98@gmail.com>
2023-09-05 21:27:12 +0900

    コードをまとまりよく変更

--- Ruby/ls.rb
+++ Ruby/ls.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+INITIAL_COLUMN = 3
+
 def parse_file
   Dir.glob('*').sort
 end
@@ -11,19 +13,17 @@ def calculate_row_and_space(all_files)
   [total_row, width]
 end
 
-INITIAL_COLUMN = 3
-all_files = parse_file
-total_row, width = calculate_row_and_space(all_files)
-
 def ls(all_files, total_row, width)
-  all_sort_file = all_files.each_slice(total_row).to_a
+  all_sort_files = all_files.each_slice(total_row).to_a
   total_row.times do |col|
-    initial_row.times do |row|
-      file_name = all_sort_file[row][col]
+    INITIAL_COLUMN.times do |row|
+      file_name = all_sort_files[row][col]
       print file_name.ljust(width) unless file_name.nil?
     end
     puts
   end
 end
 
+all_files = parse_file
+total_row, width = calculate_row_and_space(all_files)
 ls(all_files, total_row, width)
