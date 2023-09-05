d566cc1dc109a81c0486d08e387b1a130d0ca93e
reckyy <srreki98@gmail.com>
2023-09-05 21:24:51 +0900

    initial_rowをINITIAL_COLUMNに変更

--- Ruby/ls.rb
+++ Ruby/ls.rb
@@ -4,18 +4,18 @@ def parse_file
   Dir.glob('*').sort
 end
 
-def calculate_row_and_space(initial_row, all_files)
-  div, mod = all_files.size.divmod(initial_row)
+def calculate_row_and_space(all_files)
+  div, mod = all_files.size.divmod(INITIAL_COLUMN)
   total_row = mod.zero? ? div : (div + 1)
   width = all_files.max_by(&:length).length + 7
   [total_row, width]
 end
 
-initial_row = 3
+INITIAL_COLUMN = 3
 all_files = parse_file
-total_row, width = calculate_row_and_space(initial_row, all_files)
+total_row, width = calculate_row_and_space(all_files)
 
-def ls_v1(initial_row, all_files, total_row, width)
+def ls_v1(all_files, total_row, width)
   all_sort_file = all_files.each_slice(total_row).to_a
   total_row.times do |col|
     initial_row.times do |row|
@@ -26,13 +26,13 @@ def ls_v1(initial_row, all_files, total_row, width)
   end
 end
 
-def ls_v2(initial_row, all_files, total_row, width)
+def ls_v2(all_files, total_row, width)
   grouped_files = all_files.map.with_index do |file, idx|
     [idx % total_row, file]
   end
   grouped_files = grouped_files.group_by { |div, _| div }
   total_row.times do |row|
-    initial_row.times do |col|
+    INITIAL_COLUMN.times do |col|
       file_name = grouped_files[row][col]
       print file_name[1].ljust(width) unless file_name.nil?
     end
@@ -40,5 +40,5 @@ def ls_v2(initial_row, all_files, total_row, width)
   end
 end
 
-ls_v1(initial_row, all_files, total_row, width)
-ls_v2(initial_row, all_files, total_row, width)
+ls_v1(all_files, total_row, width)
+ls_v2(all_files, total_row, width)
